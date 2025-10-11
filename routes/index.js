69cb41f74562530b2925635e729cf7ffee813a81
dwikarunia0001 <dwikarunia14157375@gmail.com>
2025-10-11 22:53:40 +0700

    add todo router in index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const userRoutes = require('./user.router');
+const todoRoutes = require('./todo.router');
 
 router.use("/users", userRoutes);
+router.use("/todos", todoRoutes);
 
 module.exports = router;
