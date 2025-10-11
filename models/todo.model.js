8fc3e6397174fa8949c1b3f7e4d78d0599f2c608
dwikarunia0001 <dwikarunia14157375@gmail.com>
2025-10-11 22:40:32 +0700

    make Todo model

---
+++ models/todo.model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const todoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'Title is required'],
+    trim: true,
+    maxlength: [100, 'Title cannot exceed 100 characters']
+  },
+  description: {
+    type: String,
+    default: ''
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Update updatedAt setiap kali todo diubah
+todoSchema.pre('save', function (next) {
+  this.updatedAt = Date.now();
+  next();
+});
+
+module.exports = mongoose.model('Todo', todoSchema);
