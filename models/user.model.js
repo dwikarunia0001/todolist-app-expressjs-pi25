a829eace604872a84c19b4dab9bbfecd20e6dd3d
dwikarunia0001 <dwikarunia14157375@gmail.com>
2025-10-11 20:21:22 +0700

    make User model

---
+++ models/user.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: [true, 'Username is required'],
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+        lowercase: true,
+        match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*\.\w{2,}$/i, 'Please enter a valid email']
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        minlength: [6, 'Password must be at least 6 characters']
+    }
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
