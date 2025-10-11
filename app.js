33e0bfc6dff25b1e4002b97244106d045990161f
dwikarunia0001 <dwikarunia14157375@gmail.com>
2025-10-11 21:21:54 +0700

    connect routes and database in app.js

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const allRoutes = require('./routes');
+const connectToDb = require('./config/db');
 
+dotenv.config();
 const PORT = process.env.PORT || 3000;
+connectToDb();
+
+app.use(allRoutes);
 
 // Route dasar untuk cek apakah server hidup
 app.get('/', (req, res) => {
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const userRoutes = require('./user.router');
 
-router.use("/users", verifyToken, userRoutes);
+router.use("/users", userRoutes);
 
 module.exports = router;
