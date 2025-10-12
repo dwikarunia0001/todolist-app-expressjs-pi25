fa2ddd67f68a2ca0822c1a8d068709b3b13c329f
dwikarunia0001 <dwikarunia14157375@gmail.com>
2025-10-12 21:21:58 +0700

    call auth router in index routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const authRoutes = require('./auth.router');
 const userRoutes = require('./user.router');
 const todoRoutes = require('./todo.router');
 
+router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/todos", todoRoutes);
 
